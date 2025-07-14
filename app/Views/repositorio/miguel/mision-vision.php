e1d7aed40ebfbdf1985b5233c9e360f5a26cac45
MiguelUmbarila <umbarilamiguel2910@gmail.com>
2025-07-13 23:26:44 -0500

    modified:   app/Views/repositorio/miguel/descargaArchivos.php 	modified:   app/Views/repositorio/miguel/head.php 	modified:   app/Views/repositorio/miguel/mision-vision.php 	modified:   app/Views/repositorio/miguel/poblaciones.php 	renamed:    public/Malas_Hierbas.jpg -> public/img/Malas_Hierbas.jpg 	renamed:    public/PMJ MOSQUERA-11.png.svg -> public/img/PMJ MOSQUERA-11.png.svg 	renamed:    public/PMJ_MOSQUERA-12.png.svg -> public/img/PMJ_MOSQUERA-12.png.svg 	new file:   public/img/comunidadCuatro.jpg 	new file:   public/img/prueba.jpg

--- app/Views/repositorio/miguel/mision-vision.php
+++ app/Views/repositorio/miguel/mision-vision.php
@@ -1,26 +1,18 @@
  <!-- Mision y Vision -->
-    <div class="mx-auto max-w-md overflow-hidden rounded-xl bg-white shadow-md md:max-w-2xl">
-        <div class="md:flex">
-            <div class="md:shrink-0">
-                <img class="h-48 w-full object-cover sm:w-48 lg:h-48 " src="/PMJ MOSQUERA-11.png.svg" alt="Modern building architecture"/>
-            </div>
-            <div class="p-8">
-                <div class="text-xl font-semibold tracking-wide text-indigo-500 uppercase">Mision</div>
-                <p class="mt-2 text-gray-500">La organización Malas Hierbas tiene como finalidad promover la garantía de derechos hacia las mujeres, diversidades sexuales y de género, a partir de la generación de iniciativas que fortalezcan las capacidades de cada uno de los sectores poblacionales y la articulación conjunta con colectivas, instituciones y/o entes con objetivos a fines a los de esta organización.</p>
-            </div>
+  <section class="grid place-items-center mx-auto w-full">
+    <div class="flex flex-col items-center bg-gray-800 border border-gray-200 rounded-lg shadow-sm md:flex-row md:max-w-3xl m-8 p-3">
+        <img class="object-cover w-full rounded-lg h-96 md:h-auto md:w-80 md:rounded-[100] " src="/img/prueba.jpg" alt="">
+        <div class="flex flex-col justify-between p-4 leading-normal">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Mision</h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">La organización Malas Hierbas tiene como finalidad promover la garantía de derechos hacia las mujeres, diversidades sexuales y de género, a partir de la generación de iniciativas que fortalezcan las capacidades de cada uno de los sectores poblacionales y la articulación conjunta con colectivas, instituciones y/o entes con objetivos a fines a los de esta organización.</p>
         </div>
     </div>
 
-    <    <!-- Mision y Vision -->
-    <div class="mx-auto max-w-md overflow-hidden rounded-xl bg-white shadow-md md:max-w-2xl">
-        <div class="md:flex">
-            <div class="md:shrink-0">
-                <img class="h-48 w-full object-cover sm:w-48 lg:w-48 " src="/PMJ MOSQUERA-11.png.svg" alt="Modern building architecture"/>
-            </div>
-            <div class="p-8">
-                <div class="text-xl font-semibold tracking-wide text-indigo-500 uppercase">Vision</div>
-                <p class="mt-2 text-gray-500">Ser una organización referente en la promoción y defensa de los derechos de las mujeres, diversidades sexuales y de género, articulando con colectivas, instituciones y organizaciones afines para construir una sociedad más justa, inclusiva y equitativa, mediante el fortalecimiento de capacidades y el impulso de iniciativas transformadoras.</p>
-            </div>
+    <div class="flex flex-col items-center bg-gray-800 border border-gray-200 rounded-lg shadow-sm md:flex-row md:max-w-3xl m-8 p-3">
+        <div class="flex flex-col justify-between p-4 leading-normal">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Vision</h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Ser una organización referente en la promoción y defensa de los derechos de las mujeres y de las diversidades sexuales y de género, construyendo una sociedad más justa, equitativa e inclusiva a través del fortalecimiento comunitario, la acción colectiva y el trabajo articulado con otros espacios comprometidos con la transformación social.</p>
         </div>
-    </div>        
+        <img class="object-cover w-full rounded-lg h-96 md:h-auto md:w-80 md:rounded-[100]" src="/img/prueba.jpg" alt="">
+    </div>      
 </section>
